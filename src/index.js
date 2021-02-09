783b7166d3e2b2e26fcff3f91d498f7bf777d712
morganfula <morgan.fula@me.com>
2021-02-09 20:01:59 +0100

    js

--- src/index.js
+++ src/index.js
@@ -8,3 +8,17 @@ nav.addEventListener('click', () => {
     menu.style.display = 'grid';
   }, 200);
 });
+
+const ListLi = document.querySelectorAll('.menu ul li');
+
+console.log(ListLi);
+
+ListLi.forEach((li) => {
+  li.addEventListener('mouseenter', () => {
+    console.log('mouseenter', li);
+  });
+
+  li.addEventListener('mouseleave', () => {
+    console.log('mouseleave', li);
+  });
+});
